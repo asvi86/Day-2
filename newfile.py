40d90d92fc5511e3f05427fa5358cfa8f88002c3
asvi86 <85801374+asvi86@users.noreply.github.com>
2021-06-13 16:59:19 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,26 @@
+print  ("30 hours 30 days")
+hours = "thirty"
+print (hours)
+Days = "Thirty days"
+print  (Days[3])
+challenge = " I will win"
+print   (challenge[7:11])
+challenge = "I will win "
+print(len(challenge))
+a = "30 days"
+b = "30 hours"
+c = a+b
+print (c)
+x = "Best"
+y = " Enlist"
+e = x+" "+y
+print(e)
+text = " thirty days thirty hours"
+s = text.casefold()
+print(s)
+r = text.capitalize()
+print (r)
+s = text.isalpha()
+print (s)
+s = text.isalnum()
+print (s)
